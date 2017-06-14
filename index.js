3f9fa9c1b3343e8f5443cc134fa9d54c84636dc8
Melissa <melissay94@yahoo.com>
2017-06-14 16:03:02 -0400

    Finished with initial redesign

--- index.js
+++ index.js
@@ -30,8 +30,24 @@ const loadData = function() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
         centerMode: true,
         centerPadding: '0px',
+        responsive: [
+          {
+            breakpoint: 1024, 
+            settings: {
+              slidesToShow: 2,
+            }
+          }, 
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+        ],
       });
     });
   }
