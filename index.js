5ff780e42dcfd6cd1a454b2bfb38df62d208bf61
Melissa Young <melissay94@yahoo.com>
2017-09-22 00:40:41 -0400

    Got tired of the carousel, so I removed it

--- index.js
+++ index.js
@@ -17,46 +17,19 @@ const loadData = function() {
       // Put all the fields into elements and add it to the html
       html += "<div class='slide' style='background: #eee url(" + project.picture +") no-repeat center;";
       html += " background-size: 100% auto;'>";
+      html += "<div class='test'><div class='row'><a href=" + project.link + " target='_blank'>";
+      html += "<div class='col-md-6'><h3>" + project.title + "</h3>";
       if (project.github != "") {
         html += "<a href=" + project.github + " target='_blank'>";
         html += "<span class='fa-stack'><i class='fa fa-circle fa-stack-2x'></i>";
         html += "<i class='fa fa-github-alt fa-stack-1x'></i></span></a>";
       }
-      html += "<div class='test'><div class='row'><a href=" + project.link + " target='_blank'>";
-      html += "<div class='col-md-6'><h3>" + project.title + "</h3></div>";
+      html+= "</div>";
       html += "<div class='col-md-6'><p>" + project.tagline + "</p></div>";
-      html += "</a></div></div></div>";
+      html += "</a></div></div></div>"; 
     }
 
     document.querySelector('#portfolio').innerHTML = html;
-
-    $(document).ready(function() {
-      $('.my-slides').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 6000,
-        centerMode: true,
-        centerPadding: '0px',
-        arrows: false,
-        dots: true,
-        responsive: [
-          {
-            breakpoint: 1201, 
-            settings: {
-              slidesToShow: 2,
-            }
-          }, 
-          {
-            breakpoint: 545,
-            settings: {
-              slidesToShow: 1,
-            }
-          }
-        ],
-      });
-    });
   }
 
   xhr.open('GET', "projects.JSON", true);
