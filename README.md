353866094ba38f7d609cb12992ebec51becf47b8
Melissa Young <melissay94@yahoo.com>
2020-04-19 22:52:36 -0700

    Started about section, needs more styling

--- README.md
+++ README.md
@@ -3,6 +3,19 @@
 Link: http://melissay94.github.io
 
 ## Currently using the following: 
-+ FontAwesome 
-+ Sass
-+ Bootstrap 4.0.0
++ React
++ ReactStrap
+
+
+## Image Sources
+[HTML Logo](https://www.stickpng.com/assets/images/5847f5bdcef1014c0b5e489c.png)
+[CSS Logo](https://cdn.freebiesupply.com/logos/large/2x/css-3-logo-png-transparent.png)
+[JS Logo](https://i7.pngguru.com/preview/725/775/808/javascript-html-logo-blog-css3-javanese.jpg)
+[React Logo](https://cdn.clipart.email/37fe3ea3b3224b13bfee0d4158266c38_react-logo-png-download-500500-free-transparent-react-png-_900-500.jpeg)
+[BootStrap Logo](https://cdn.freebiesupply.com/logos/large/2x/bootstrap-4-logo-png-transparent.png)
+[Material UI Logo](https://material-ui.com/static/logo.png)
+[PhaserJS Logo](https://www.pngitem.com/pimgs/m/184-1843034_phaser-phaser-3-logo-hd-png-download.png)
+[Node Logo](https://p7.hiclipart.com/preview/306/37/167/node-js-javascript-web-application-express-js-computer-software-others.jpg)
+[Express Logo](https://p7.hiclipart.com/preview/545/451/583/node-js-express-js-javascript-solution-stack-web-application-others.jpg)
+[Sequelize Logo](https://miro.medium.com/max/661/1*XcE0wR1ZmWLFbdF2dE5WuA.png)
+[Postgres Logo](https://portworx.com/wp-content/uploads/2018/05/postgresql-logo.png)
--- react-redo/src/sections/About.js
+++ react-redo/src/sections/About.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
+import Map from '../components/Map';
+import BootstrapLogo from '../assets/bootstrapLogo.png';
+import CssLogo from '../assets/cssLogo.png';
+import ExpressLogo from '../assets/expressLogo.png';
+import HtmlLogo from '../assets/htmlLogo.png';
+import JsLogo from '../assets/jsLogo.png';
+import MaterialUILogo from '../assets/materialUILogo.png';
+import NodeJsLogo from '../assets/nodeJsLogo.png';
+import PhaserLogo from '../assets/phaserLogo.png';
+import PostgresLogo from '../assets/postgresLogo.png';
+import ReactLogo from '../assets/reactLogo.png';
+import SequelizeLogo from '../assets/sequelizeLogo.png';
 
 export default function About() {
   return(
-    <div></div>
+    <Row id="about">
+      <Col xs='12' lg='auto' xl='6' className='map-section'>
+        <h2>Map of Me</h2>
+        <h6>(Click on a city to learn more about me!)</h6>
+        <Map />
+      </Col>
+      <Col xs='12' lg='auto' xl='6' className='brand-statement'>
+        <h2>About Me</h2>
+        <p>I am a passionate Software Engineer focused on Front End Development, utilizing JavaScript technologies to create engaging, outward facing customer products. My past experience as an app developer solidified my understanding of strong teamwork in agile environments, as well as an appreciation for candid conversation. As a self-motivated lifelong learner, I love building my knowledge base to share with others. For example, I seek out volunteering opportunities, specifically around teaching Computer Science to the younger generation.</p>
+        <Row className='tech-logo-section'>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ HtmlLogo } alt='HTML Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ CssLogo } alt='CSS Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ JsLogo } alt='JavaScript Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ BootstrapLogo } alt='Bootstrap Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ MaterialUILogo } alt='Material UI Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ ReactLogo } alt='React Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ ExpressLogo } alt='Express Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ NodeJsLogo } alt='Node Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ PhaserLogo } alt='Phaser 3 Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ SequelizeLogo } alt='Sequelize Logo' className='tech-logo' />
+          </Col>
+          <Col xs='6' md='4' xl='3'>
+            <img src={ PostgresLogo } alt='Postgres Logo' className='tech-logo' />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 };
